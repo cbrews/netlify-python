834a29631e289e6cf815b5951b41a110268bee1b
cbrews <cbrews@users.noreply.github.com>
2025-02-14 16:44:29 -0500

    0.3.2 release (#266)

--- netlify/client.py
+++ netlify/client.py
@@ -3,7 +3,7 @@ from netlify.pydantic_polyfill import PydanticPolyfill
 from netlify.schemas import CreateSiteRequest, Site, SiteDeploy, SiteFile, User
 from netlify.transport import NetlifyTransport
 
-CLIENT_USER_AGENT = "NetlifyPythonClient/0.3.1"
+CLIENT_USER_AGENT = "NetlifyPythonClient/0.3.2"
 
 
 class NetlifyClient:
